ffc1c6c1e519075671eaad8e5f1dabefec563100
Commelina <maosics@gmail.com>
2022-04-21 17:27:02 +0800

    simple example. It does not work now. WHY? Can u help me?

---
+++ example/Main.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE OverloadedStrings  #-}
+
+module Main where
+
+import Types
+import Graph
+import Shard
+
+import Control.Concurrent
+import Control.Monad
+import Data.Aeson (Value (..))
+import Data.Word
+
+main :: IO ()
+main = do
+  let subgraph_0 = Subgraph 0
+      (builder_1, subgraph_1) = addSubgraph emptyGraphBuilder subgraph_0
+  let (builder_2, node_1) = addNode builder_1 subgraph_0 InputSpec
+      (builder_3, node_2) = addNode builder_2 subgraph_0 (OutputSpec node_1)
+  let graph = buildGraph builder_3
+
+  shard <- buildShard graph
+  forkIO $ run shard
+
+  pushInput shard node_1 (DataChange [String "aaa"] (Timestamp (0 :: Word32) []) 1)
+  pushInput shard node_1 (DataChange [String "bbb"] (Timestamp (0 :: Word32) []) 1)
+  flushInput shard node_1
+
+  threadDelay 10000000
