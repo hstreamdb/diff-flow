45af6b96fb5a6a1e0eb08988da315889f59e4689
Commelina <maosics@gmail.com>
2022-04-22 13:54:53 +0800

    minor update on the example

--- example/Main.hs
+++ example/Main.hs
@@ -17,8 +17,10 @@ main = do
   let subgraph_0 = Subgraph 0
       (builder_1, subgraph_1) = addSubgraph emptyGraphBuilder subgraph_0
   let (builder_2, node_1) = addNode builder_1 subgraph_0 InputSpec
-      (builder_3, node_2) = addNode builder_2 subgraph_0 (OutputSpec node_1)
-  let graph = buildGraph builder_3
+  let mapper = Mapper (\row -> let (String s) = head row in [String (s <> "_mapped")])
+  let (builder_3, node_2) = addNode builder_2 subgraph_0 (MapSpec node_1 mapper)
+      (builder_4, node_3) = addNode builder_3 subgraph_0 (OutputSpec node_2)
+  let graph = buildGraph builder_4
 
   shard <- buildShard graph
   forkIO $ run shard
